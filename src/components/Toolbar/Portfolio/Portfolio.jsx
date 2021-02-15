1a11d5e9d7d4a10ea3d0e810abea201185025554
zarajskysam <zarajskysam92@gmail.com>
2021-02-15 19:27:10 +0300

    commit added 2

--- src/components/Toolbar/Portfolio/Portfolio.jsx
+++ src/components/Toolbar/Portfolio/Portfolio.jsx
@@ -3,45 +3,19 @@ import Toolbar from '../Toolbar/Toolbar';
 import { useState } from 'react';
 import ProjectList from '../ProjectList/ProjectList';
 import { imgData } from '../img-data/img-data';
-import Masonry from 'masonry-layout';
 
-const masonry = () => {
-    const grid = document.querySelector('.projectlist');
-    const masonry = new Masonry(grid, {
-    itemSelector: '.projectlist-item',
-        gutter: 10,
-        originBottom: false,
-    });
-} 
+
+
+
 
 const Portfolio = () => {
-    // window.onload = () => {
-    //     masonry();
-    // }
     const [selectedItem, setSelectedItem] = useState('All');
     const [imgArray, setImgArray] = useState(imgData);
+    const [styleButton, setStyleButton] = useState('unselect');
+
 
     const onSelectFilter = (item) => {
-
-        if(item.classList.contains('select')){
-            item.classList.remove('select');
-            setSelectedItem('All');
-            filterImg('All');
-            console.log(imgData);
-            return;
-        }
-        Array.from(document.querySelectorAll('.unselect')).forEach(elem => {
-            if(elem.classList.contains('select')) {
-                elem.classList.remove('select');
-                setSelectedItem('All');
-                console.log('aaa');
-                return;
-            }
-        })
-        item.classList.add('select');
-        filterImg(item.value);
-        setSelectedItem(item.value);
-        return;
+        filterImg(item);
     }
 
     const filterImg = (value) => {
@@ -54,11 +28,11 @@ const Portfolio = () => {
         setImgArray(newArr);
     }
 
+
     return (
         <div className="container">
-            <Toolbar filters={["All", "Websites", "Flayers", "Business Cards"]} onSelectFilter={onSelectFilter} selected={selectedItem} data-selected={selectedItem}/>
+            <Toolbar buttonStyle={styleButton} filters={["All", "Websites", "Flayers", "Business Cards"]} onSelectFilter={onSelectFilter} selected={selectedItem} data-selected={selectedItem}/>
             <ProjectList options={imgArray}/>
-            {/* {masonry} */}
         </div>    
     )
 }
--- src/components/Toolbar/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar/Toolbar.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './toolbar.css';
-import Masonry from 'masonry-layout';
+
 
 
 const Toolbar = (props) => {
-    const { filters, selected, onSelectFilter } = props;
-    const selectToolbar = (e) => {
-        onSelectFilter(e.target);
-    }
+    const { filters, selected, onSelectFilter, buttonStyle } = props;
+    
     return (
         filters.map(filter => (
-            <button className='unselect' onClick={selectToolbar} selected={selected} data-selected={selected} key={filter} value={filter}>{filter}</button>
+            <button className={buttonStyle} onClick={() => onSelectFilter(filter)} selected={selected} data-selected={selected} key={filter} value={filter}>{filter}</button>
         ))
     )
 }
